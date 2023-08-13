9f36a693ecb7e0ef6b1d10e6f8937b8f4af79ce3
Manar Albogha <100355986+manaralbogha@users.noreply.github.com>
2023-08-13 17:53:12 +0300

    commit

--- lib/modules/add_property_screen/widgets/selected_images_list.dart
+++ lib/modules/add_property_screen/widgets/selected_images_list.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:untitled/modules/add_property_screen/cubit/add_property_cubit.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
 
 class SelectedImagesList extends StatelessWidget {
   final AddPropertyCubit cubit;
@@ -19,15 +21,53 @@ class SelectedImagesList extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               itemCount: cubit.selectedImagesList.length,
               itemBuilder: (BuildContext context, int index) {
-                return Container(
-                  width: 160,
-                  height: 160,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(25.r),
-                    image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: FileImage(
-                        File(cubit.selectedImagesList[index].path),
+                return InkWell(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return Container(
+                          child: PhotoViewGallery.builder(
+                            scrollPhysics: const BouncingScrollPhysics(),
+                            builder: (BuildContext context, int index) {
+                              return PhotoViewGalleryPageOptions(
+                                imageProvider: FileImage(
+                                  File(cubit.selectedImagesList[index].path),
+                                ),
+
+                                initialScale:
+                                    PhotoViewComputedScale.contained * 0.8,
+                                // heroAttributes: PhotoViewHeroAttributes(tag: galleryItems[index].id),
+                              );
+                            },
+                            itemCount: cubit.selectedImagesList.length,
+                            loadingBuilder: (context, event) => Center(
+                              child: Container(
+                                width: 20.0,
+                                height: 20.0,
+                                child: CircularProgressIndicator(
+                                  value: event == null
+                                      ? 0
+                                      : event.cumulativeBytesLoaded /
+                                          event.expectedTotalBytes!.toInt(),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    );
+                  },
+                  child: Container(
+                    width: 160,
+                    height: 160,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25.r),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: FileImage(
+                          File(cubit.selectedImagesList[index].path),
+                        ),
                       ),
                     ),
                   ),
