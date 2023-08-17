df5a32f7863bba053b72ff9abeadd6b31177c2c1
Manar Albogha <100355986+manaralbogha@users.noreply.github.com>
2023-08-17 14:48:51 +0300

    cpmmit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:untitled/modules/Search.dart';
 import 'package:untitled/modules/splash_screen/splash_screen.dart';
 
 import 'package:untitled/shared/network/local/cache_helper.dart';
@@ -53,6 +54,7 @@ class MyApp extends StatelessWidget {
               ),
             ),
           ),
+          // home: Search(),
           initialRoute: SplashView.route,
           routes: AppRouter.router,
         );
